90fdb1c8028b48297a446671fabf80f59d365d9d
jaehyunjang <hyuni2020@gmail.com>
2023-02-15 17:50:23 +0900

    TextView modifier (first commit)

---
+++ app/src/main/java/com/compose/composebase/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.compose.composebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.compose.composebase.ui.theme.ComposeBaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    Greeting("이거 좋은데?")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        modifier = Modifier.size(300.dp),
+        color = Color(0xffff9944),
+        text = "Hello $name!! Hello $name!!",
+        fontSize = 50.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        letterSpacing = 2.sp,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeBaseTheme {
+        Greeting("world")
+    }
+}
