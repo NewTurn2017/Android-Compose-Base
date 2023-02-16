9a71f3288e084a88234a18a029f8decbb903ce34
jaehyunjang <hyuni2020@gmail.com>
2023-02-16 14:05:58 +0900

    Image

--- app/src/main/java/com/compose/composebase/MainActivity.kt
+++ app/src/main/java/com/compose/composebase/MainActivity.kt
@@ -6,11 +6,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -18,10 +15,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -36,43 +36,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBaseTheme {
-                Outer()
+                Greeting()
+
             }
         }
     }
 }
 
 @Composable
-fun Outer() {
+fun Greeting() {
     Column {
-        Inner(
-            modifier = Modifier
-                .widthIn(min = 100.dp, max = 350.dp)
-                .heightIn(min = 160.dp)
+        Image(
+            painter = painterResource(id = R.drawable.wall),
+            contentDescription = "엔텔로프 캐년",
+        )
+
+        Image(
+            imageVector = Icons.Filled.Settings,
+            contentDescription = "설정",
         )
     }
-
 }
 
-@Composable
-fun Inner(modifier: Modifier = Modifier) {
-    BoxWithConstraints(modifier) {
-        if (maxHeight > 150.dp) {
-            Text(
-                text = "여기 꽤 길군요.",
-                modifier = Modifier.align(Alignment.BottomCenter),
-            )
-        }
-        Text("maxW: ${maxWidth}, maxH: ${maxHeight}, minW: ${minWidth}, minH: $minHeight")
-    }
-
-}
-
-
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeBaseTheme {
-        Outer()
+        Greeting()
     }
 }
